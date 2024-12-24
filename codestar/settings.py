ab717785c8077bd399138873a990aceb2490e572
Joesph Afonne <josef.afonne@gmail.com>
2024-12-24 11:08:16 +0000

    Revert url back

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-joemburu-codestar-g3masjju0w4.ws.codeinstitute-ide.net','.herokuapp.com', 'localhost']
 
