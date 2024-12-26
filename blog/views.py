cbc280cf63d1dd15773d30436e791e330aa6bffc
Joesph Afonne <josef.afonne@gmail.com>
2024-12-26 16:02:50 +0000

    Add home page template

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,7 @@ from .models import Post
 class PostList(generic.ListView):
     #queryset = Post.objects.all()
     queryset = Post.objects.all()
-    template_name = 'post_list.html'
+    #template_name = 'post_list.html'
+    template_name = 'blog/index.html'
+    paginate_by = 6
+    
