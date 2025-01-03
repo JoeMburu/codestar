e8d7ac135c448338c1b4bc558bf8f962e90ec994
Joesph Afonne <josef.afonne@gmail.com>
2025-01-03 20:49:47 +0000

    posting and writing to database

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
+from django.contrib import messages
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -30,6 +32,21 @@ def post_detail(request, slug):
     comments = post.comments_based_on_post.all().order_by('-created_on')
     comment_count = post.comments_based_on_post.filter(approved=True).count()
 
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.author = request.user
+            new_comment.post = post
+            new_comment.save()
+            messages.add_message(
+                request, 
+                messages.SUCCESS, 
+                'Comment submitted and awaiting approval'
+            )
+
+    comment_form = CommentForm()
+
     return render(
         request, 
         'blog/post_detail.html', 
@@ -37,5 +54,6 @@ def post_detail(request, slug):
             "post": post , 
             "comments": comments,
             "comment_count": comment_count, 
+            "comment_form": comment_form,
         },
     )
